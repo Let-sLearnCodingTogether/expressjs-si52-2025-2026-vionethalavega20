173a6c57b3860793c38af69561d614170c386b34
vionethalavega20 <vionetha2085@gmail.com>
2025-09-20 18:46:55 +0700

    setup expressjs project (pertemuan 3)

---
+++ routes/web.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const web = express.Router()
+
+web.get('/', (req, res) => {
+    res.render('index')
+})
+
+web.get('/:username', (req,res) => {
+    res.render('public-profile')
+})
+
+export default web 
