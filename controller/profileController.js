ce273ddcb655a583b496104fd8a6408d5b825d3e
vionethalavega20 <vionetha2085@gmail.com>
2025-10-22 20:36:04 +0700

    create middleware, private profile & its setup

--- controller/profileController.js
+++ controller/profileController.js
@@ -5,6 +5,18 @@ export const publicProfile = (req,res) => {
         username: username,
         bio : "test 123"
     })
-
-
 }
+
+export const privateProfile = async(req,res) => {
+    try {
+        res.status(200).json({
+            message: "Private profile berhasil di akses",
+            data: null
+        })
+    } catch(error) {
+        res.status(500).json({
+            message: error.message,
+            data: null
+        });
+    }
+};
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import passport from "passport";
+
+export const protect = (req,res,next) => {
+    passport.authenticate(
+        'jwt', 
+        {
+        session: false
+        }, 
+        (err, user, info) => {
+            if (err || !user) {
+                return res.status(401).json({
+                    message: info? info.message : "Unauthorized",
+                    err: err || "Tidak valid",
+                });
+            }
+
+            req.user = user;
+
+            return next(); //untuk mengizinkan user tersebut untuk lewat 
+        }
+    )(req, res, next); 
+}
