91951b6685a033b4f26fdfa09b74073eb0a75d1b
vionethalavega20 <vionetha2085@gmail.com>
2025-10-25 18:05:15 +0700

    update profile controller

--- controller/profileController.js
+++ controller/profileController.js
@@ -9,9 +9,11 @@ export const publicProfile = (req,res) => {
 
 export const privateProfile = async(req,res) => {
     try {
+        const user = req.user;
+
         res.status(200).json({
             message: "Private profile berhasil di akses",
-            data: null
+            data: user
         })
     } catch(error) {
         res.status(500).json({
